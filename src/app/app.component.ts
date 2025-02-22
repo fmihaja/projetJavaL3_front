c579858f196d7e854791e1016fad8af01f95145e
theLastAirBender <fmihaja041@gmail.com>
2025-02-22 21:33:33 +0100

    formulaire connexion fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+// import { IconFieldModule } from 'primeng/iconfield';
+// import { InputIconModule } from 'primeng/inputicon';
+// import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ButtonModule],
+  imports: [
+    RouterOutlet, ButtonModule,
+    InputTextModule, FloatLabelModule,
+    // IconFieldModule, InputIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
