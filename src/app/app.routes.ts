ec683cc1a259ef4171fd0a9ffeefa8cb6e7e1443
theLastAirBender <fmihaja041@gmail.com>
2025-02-23 15:32:13 +0100

    routes clients fait

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { HomeComponent } from '../components/home/home.component';
 import { IndexComponent } from '../components/home/index/index.component';
+import { ClientComponent } from '../components/home/client/client.component';
 
 export const routes: Routes = [
     { path:'',redirectTo:'login', pathMatch:'full'},
@@ -9,7 +10,8 @@ export const routes: Routes = [
     {   path:'home',
         component: HomeComponent,
         children:[
-            {path:'', component:IndexComponent}
+            {path:'', component: IndexComponent},
+            {path:'client', component: ClientComponent}
         ]
     },
 
