9f1f1027033cc22783f8504f9664e022b18c9ac5
theLastAirBender <fmihaja041@gmail.com>
2025-02-23 11:02:43 +0100

    ajout dashboard fait

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -3,16 +3,17 @@ import { ToolbarModule } from 'primeng/toolbar';
 // import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { DrawerModule } from 'primeng/drawer';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
   imports: [
     ToolbarModule, ButtonModule,
-    DrawerModule
+    DrawerModule, CardModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  visible2: boolean = false;
+  settingNav: boolean = false;
 }
