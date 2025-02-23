342a427bc43abfc11269bc69d4ce8e2dd578ba2c
theLastAirBender <fmihaja041@gmail.com>
2025-02-23 19:08:18 +0100

    pop up fait

--- src/components/home/client/client.component.ts
+++ src/components/home/client/client.component.ts
@@ -4,18 +4,22 @@ import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { ButtonModule } from 'primeng/button';
+import { AutoFocusModule } from 'primeng/autofocus';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-client',
   imports: [
     TableModule, InputTextModule,
     IconFieldModule, InputIconModule,
-    ButtonModule
+    ButtonModule, AutoFocusModule,
+    DialogModule
   ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
 export class ClientComponent {
+  modal: boolean=false;
   products=[
     {
       code:600,
