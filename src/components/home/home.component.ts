15a225468a40e01b326454e06e5f95d563fe5f3b
theLastAirBender <fmihaja041@gmail.com>
2025-02-23 08:34:13 +0100

    navbar fait

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+// import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    ToolbarModule, ButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
