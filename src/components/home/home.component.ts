f89f05f5166031eeab6df4bae77848bd7d272d73
theLastAirBender <fmihaja041@gmail.com>
2025-02-23 10:07:26 +0100

    ajout setting NavBar

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 // import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-home',
   imports: [
-    ToolbarModule, ButtonModule
+    ToolbarModule, ButtonModule,
+    DrawerModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  visible2: boolean = false;
 }
