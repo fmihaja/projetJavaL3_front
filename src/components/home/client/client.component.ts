2cf60d5c023fc99b4f7bd89ab873b5f777299350
theLastAirBender <fmihaja041@gmail.com>
2025-02-23 18:36:30 +0100

    ajout icone sur input fait

--- src/components/home/client/client.component.ts
+++ src/components/home/client/client.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [
+    TableModule, InputTextModule,
+    IconFieldModule, InputIconModule
+  ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
 export class ClientComponent {
-
+  products=[
+    {
+      code:600,
+      name:"test",
+      category:"poids lourds",
+      quantity:500
+    },
+    {
+      code:500,
+      name:"test3",
+      category:"poids leger",
+      quantity:600
+    },
+    {
+      code:900,
+      name:"test5",
+      category:"aoids lourds",
+      quantity:700
+    },
+  ]
 }
