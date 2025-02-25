06d5591c36b0b8ece2a6a5d9f38368f9a786ef6e
theLastAirBender <fmihaja041@gmail.com>
2025-02-25 20:12:24 +0100

    Add chart component and update UI for quantity display

--- src/components/home/index/index.component.ts
+++ src/components/home/index/index.component.ts
@@ -1,12 +1,88 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { ChartModule } from 'primeng/chart';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectorRef, OnInit, PLATFORM_ID, inject } from '@angular/core';
 
 @Component({
   selector: 'app-index',
-  imports: [ButtonModule],
+  imports: [
+    ButtonModule, ChartModule
+  ],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrls: ['./index.component.css']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  data: any;
+  options: any;
 
+  platformId = inject(PLATFORM_ID);
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.initChart();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+      const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+      this.data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'black',
+            tension: 0.4
+          },
+          {
+            label: 'Second Dataset',
+            data: [28, 48, 40, 19, 86, 27, 90],
+            fill: false,
+            borderColor: documentStyle.getPropertyValue('--p-gray-500'),
+            tension: 0.4
+          }
+        ]
+      };
+
+      this.options = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder,
+              drawBorder: false
+            }
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder,
+              drawBorder: false
+            }
+          }
+        }
+      };
+      this.cd.markForCheck();
+    }
+  }
 }
