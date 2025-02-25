00964cb3370f6bc7e708b40e6b00387c095c243d
theLastAirBender <fmihaja041@gmail.com>
2025-02-25 22:06:31 +0100

    router fait

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -4,13 +4,14 @@ import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { FooterComponent } from './layout/footer/footer.component';
 import { DrawerModule } from 'primeng/drawer';
-
+import { RouterModule } from '@angular/router';
 // import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
   imports: [
     RouterOutlet, FooterComponent,
+    RouterModule,
     ToolbarModule, ButtonModule, DrawerModule 
   ],
   templateUrl: './home.component.html',
